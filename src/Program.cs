926faac31dd54f5f3dc4551cb284a3048da2c03c
Sheng Guo <shenguo@microsoft.com>
2021-11-04 12:53:46 +0800

    Add support for plain text http2. add windows dockerfile.

--- src/Program.cs
+++ src/Program.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace HelloWorldService
 {
@@ -21,9 +21,20 @@ namespace HelloWorldService
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>()
-                        .UseKestrel(options =>
+                        .UseKestrel((ctx, options) =>
                         {
                             options.Limits.MaxRequestBodySize = null;
+                            if (ctx.Configuration.GetValue<bool>("useHTTP2", true))
+                            {
+                                string http2Endpoint = ctx.Configuration.GetValue<string>("http2Endpoint");
+                                options.ConfigureEndpointDefaults(listenOptions =>
+                                {
+                                    if (string.IsNullOrEmpty(http2Endpoint) || http2Endpoint.Contains(listenOptions.IPEndPoint.ToString()))
+                                    {
+                                        listenOptions.Protocols = HttpProtocols.Http2;
+                                    }
+                                });
+                            }
                         });
                 });
     }
